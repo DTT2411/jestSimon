610f55e72f9be99880562d04bf39fd8e0be0ec97
DTT2411 <david.thomson2411@gmail.com>
2025-05-27 01:29:37 +0100

    second commit

--- scripts/game.js
+++ scripts/game.js
@@ -1,7 +1,13 @@
-// game [object]
+// game [object] score, currentGame, playerMoves, choices
 //newGame()
 //addTurn()
 //showTurns()
 //lightsOn
 //playerTurn
 //showScore()
+
+let game = {
+    score: 0,
+}
+
+module.exports = {game};
